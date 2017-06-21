262818d541944a4045d447a45850b5ffcbc9b33a
tianchi.gzt <tianchi.gzt@taobao.com>
2017-06-21 21:32:08 +0800

    修改

--- Python/TestPandas.py
+++ Python/TestPandas.py
@@ -1,13 +1,5 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 
-index = pd.date_range('10/1/1999', periods=1100)
-ts = pd.Series(np.random.normal(0.5, 2, 1100), index)
-ts = ts.rolling(window=100, min_periods=100).mean().dropna()
-
-key = lambda x: x.year
-zscore = lambda x : (x - x.mean()) / x.std()
-
-result = ts.groupby(key).transform(zscore)
-
-print(result)
