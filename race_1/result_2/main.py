ac2590a4920dd3d2e30b3bbbf08fb74fc9f2ffed
tianchi.gzt <tianchi.gzt@taobao.com>
2017-07-09 08:00:01 +0800

    找到范围

--- race_1/result_2/main.py
+++ race_1/result_2/main.py
@@ -1,5 +1,3 @@
 import numpy as np
 
-x = np.linspace(0, 2, 1000)
-
-print(np.frompyfunc(lambda i: i + 1, 1, 1)(x))
+print(np.add.reduce([[1,2,3],[4,5,6]], axis=1))
--- race_1/result_2/process.py
+++ race_1/result_2/process.py
@@ -5,30 +5,30 @@ from matplotlib import pyplot as plt
 import time
 
 start_time = time.time()
-content = read("/Users/tianchi.gzt/Downloads/preliminary/preliminary/after/GS.WXT.2008213000000.BHZ")
+content = read("/Users/tianchi.gzt/Downloads/preliminary/preliminary/after/XX.MXI.2008214000000.BHE")
 print(time.time() - start_time)
 content.plot(type='dayplot')
 
-data = content[0].data
-
-interval = 5
-step = 600
-
-x = np.arange(0, step * interval, interval)
-xx = np.arange(step * interval)
-
-for i in np.arange(0, len(data), step * interval):
-    y = np.zeros(step)
-    max_y = 0
-
-    for j in np.arange(step):
-        y[j] = np.std(data[i + j * interval:i + (j + 1) * interval])
-        max_y = max(y[j], max_y)
-
-    # print(max_y)
-
-    if max_y > 500:
-        print(i, max_y)
-        plt.plot(xx, data[i:i + step * interval])
-        plt.plot(x, y)
-        plt.show()
+# data = content[0].data
+#
+# interval = 5
+# step = 600
+#
+# x = np.arange(0, step * interval, interval)
+# xx = np.arange(step * interval)
+#
+# for i in np.arange(0, len(data), step * interval):
+#     y = np.zeros(step)
+#     max_y = 0
+#
+#     for j in np.arange(step):
+#         y[j] = np.std(data[i + j * interval:i + (j + 1) * interval])
+#         max_y = max(y[j], max_y)
+#
+#     # print(max_y)
+#
+#     if max_y > 500:
+#         print(i, max_y)
+#         plt.plot(xx, data[i:i + step * interval])
+#         plt.plot(x, y)
+#         plt.show()
