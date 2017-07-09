b6a84f2ddbc9c8e8914ed3a9db0a590da63656c7
tianchi.gzt <tianchi.gzt@taobao.com>
2017-07-09 23:06:03 +0800

    调整特征

--- race_1/result_2/generate_kmeans.py
+++ race_1/result_2/generate_kmeans.py
@@ -25,15 +25,15 @@ if __name__ == "__main__":
     # 读取数据
     feature_list = np.array(read_feature_file())
 
-    for i in feature_list:
-        if np.isnan(i).any():
-            print(i)
+    # for i in feature_list:
+    #     if np.isnan(i).any():
+    #         print(i)
 
     # 加载模型
 
 
     # 构造聚类
-    kmeans = KMeans(n_clusters=5, random_state=0).fit(feature_list)
+    kmeans = KMeans(n_clusters=10, random_state=0, algorithm="full").fit(feature_list)
 
     # 保存模型
     joblib.dump(kmeans, './data/kmeans')
