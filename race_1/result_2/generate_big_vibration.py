4f2124d478a3e310cdfca84423e15b4383ee3a57
tianchi.gzt <tianchi.gzt@taobao.com>
2017-07-31 21:24:07 +0800

    添加注释

--- race_1/result_2/generate_big_vibration.py
+++ race_1/result_2/generate_big_vibration.py
@@ -1,4 +1,11 @@
-# 找到震动幅度骄傲的的位置，方便以后找数据，相当于是建索引吧。
+# 找到特征位置
+#
+# 1. 遍历after文件
+# 2. 计算长度为step(5)的标准差，作为振幅
+# 3. 找到振幅超过阈值vibration_limit(600)的位置，记录下来
+# 4. 在找到振幅较大的点之后，做一个长度为interval(1000)的跳跃，因为震动附近震动都比较大，没有必要把所有震动大的地方都记录下来
+# 5. 将结果写入文件./data/big_vibration.txt，格式为：文件名|位置数组JSON字符串
+
 import os
 import numpy as np
 import json
@@ -12,6 +19,7 @@ vibration_limit = 600
 result = open("./data/big_vibration.txt", "a")
 
 
+# 处理单个文件
 def process(dir_path, file_path):
     print(file_path)
     start_time = time.time()
@@ -37,7 +45,7 @@ def process(dir_path, file_path):
         result.write(file_path + "|" + json.dumps(point_list) + "\n")
         result.flush()
 
-    print("[LEN]", len(point_list), "[RET]",point_list)
+    print("[LEN]", len(point_list), "[RET]", point_list)
     print('[COST] ' + str(time.time() - start_time))
 
 
